7dfa9564e2e4ffdb8803a1db48da518390c465f2
Ailin Romano <ailin.romano@hotmail.com>
2023-10-06 11:57:21 -0300

    incorporación documentación e imágenes

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Parcial domiciliario SPD 
-holis
+# Primer parcial domiciliario SPD
+
+## Integrantes: 
+- Patricio Pereyra
+- Ailin Romano
+
+## Proyecto: contador de 0 a 99
+![img tinkercad](./img/contador_tinkercad.png)
+
+En este proyecto se diseñó un contador de 0 a 99 en una placa de arduino con dos display de 7 segmentos multiplexados. El mismo contiene tres botones. El primero aumenta el contador en una unidad, el segundo disminuye la cuenta en una unidad y el tercero resetea el contador en cero. 
+
+## Funciones:
+
+### setup()
+
+- Esta función se ejecuta una vez al inicio del programa. Configura los pines utilizados en el circuito, tanto para la salida de datos a los displays como para la entrada de los pulsadores. Los pulsadores se configuran con resistencias pull-up internas para su detección.
+
+### loop()
+
+- Función principal que se ejecuta en un bucle continuo después de la configuración inicial. Controla la lógica principal del programa, que incluye alternar entre la visualización de unidades y decenas en los displays y gestionar los pulsadores para aumentar, disminuir o restablecer los valores mostrados en los displays.
+
+### ManipularBotones()
+
+- Esta función verifica el estado de los pulsadores y actualiza las variables que rastrean su estado anterior. Se utiliza para detectar cuándo se presionan los pulsadores de subir, bajar y restablecer.
+
+### Subir(int pulsador, int estadoAntBoton)
+
+- Esta función se encarga de incrementar el valor de las unidades o decenas cuando se presiona el botón de subir. Si el valor de las unidades llega a 10, se reinicia a cero y se incrementa el valor de las decenas.
+
+### Bajar(int pulsador, int estadoAntBoton)
+
+- Esta función disminuye el valor de las unidades o decenas cuando se presiona el botón de bajar. Si el valor de las unidades llega a 0, se establece en 9 y se decrementa el valor de las decenas.
+
+### Resetear(int pulsador, int estadoAntBoton)
+
+- Cuando se presiona el botón de restablecer, esta función restablece los valores de unidades y decenas a cero.
+
+### PrenderDisplay(int display, int numero)
+
+- Esta función se utiliza para encender y apagar los displays según sea necesario. Controla la lógica de la multiplexación de los displays, asegurándose de que solo uno esté encendido en un momento dado.
+
+### MostrarNumero(int numero)
+
+- Esta función se utiliza para mostrar un número en los displays LED de 7 segmentos. Configura los pines correspondientes a los segmentos A, B, C, D, E, F y G para mostrar el número proporcionado como parámetro.
+
+### ApagarDisplay()
+
+- Esta función apaga todos los segmentos de los displays, asegurando que no se muestre ningún número en ellos.
+
+Todas estas funciones trabajan juntas para crear un sistema que permita al usuario incrementar, disminuir y restablecer valores en los displays LED de 7 segmentos utilizando pulsadores.
+
+
+## Ver el proyecto
+[En este link](https://www.tinkercad.com/things/2lvHUsaZFAT?sharecode=v3u23u9ZqbUqLvU3CQkFhtTGDUxi--_OwQYGhyNlhBg) está el proyecto en la plataforma Tinkercad.
+
+## Demo en video
+Si querés ver la demo de nuestro proyecto, [podés hacerlo acá](https://www.loom.com/share/2a0bec58491e4c078003808fe90af378?sid=bac2e6a6-ef8d-4bc9-b516-eb9dc1063fea).
+
+
+
+
+
+
+
---
+++ codigo/codigo_contador_0_a_99.ino
@@ -0,0 +1,284 @@
+/*
+En este circuito utilizamos 7 conexiones para conectar dos 
+displays a la placa de Arduino. Dichos displays están interconectados
+entre sí a través de la técnica de la multiplexación. 
+Los negativos de los displays están conectados 
+en los pines 2 y 3, de forma que se pueda mostrar números de manera
+independiente en cada uno de ellos. Por ejemplo, si el display que 
+se quiere encender es el definido como DECENAS, por el pin de salida 
+del display definido como UNIDADES enviamos 5v para que la diferencia 
+de potencial sea 0 y no circule corriente, y al de DECENAS lo dejamos
+en GND para que haya diferencia de potencial y la corriente circule. 
+Alternamos el encendido y apagado de cada display con un delay muy corto
+que nos permita crear la falsa sensación de que están encendidos 
+en simultaneo y mostrar los números que queramos.
+
+*/
+
+/* Define los segmentos A de cada display con el pin 10...*/
+#define A 10
+#define B 11
+#define C 5
+#define D 6
+#define E 7
+#define F 9
+#define G 8
+
+/*Se definen los displays UNIDADES y DECENAS
+con los pines 3 y 2 respectivamente*/
+#define UNIDADES 3
+#define DECENAS 2
+
+/*Se definen los pulsadores SUBIR, BAJAR y RESETEAR
+con los pines A5, A4 y A3 respectivamente*/
+#define SUBIR A5
+#define BAJAR A4
+#define RESETEAR A3
+
+void setup()
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(UNIDADES, OUTPUT);
+  pinMode(DECENAS, OUTPUT);
+  
+  /*Utilizamos la configuración PULLUP para los pulsadores, 
+  de manera que estén conectados a la resistencia interna de la placa*/
+  pinMode(SUBIR, INPUT_PULLUP);
+  pinMode(BAJAR, INPUT_PULLUP);
+  pinMode(RESETEAR, INPUT_PULLUP);
+}
+
+
+/*Define como NO PRESIONADOS a los valores iniciales
+de estas tres variables*/
+int subirAnterior = 1;
+int bajarAnterior = 1;
+int resetearAnterior = 1;
+
+int unidades = 0;
+int decenas = 0;
+
+
+/*Enciende y apaga de manera alternativa cada display, 
+con un delay de 50 milisegundos entre un display y el otro,
+mostrando los números que deseamos como si ambos estuviesen encendidos
+simultaneamente.*/
+void loop()
+{
+  ManipularBotones();
+  PrenderDisplay(UNIDADES, unidades);
+  delay(50);
+  PrenderDisplay(DECENAS, decenas);
+  delay(50);
+}
+
+
+/*Chequea el estado de los pulsadores utilizando las funciones
+Subir(), Bajar() y Resetear(). Actualiza los valores de las variables
+subirAnterior, bajarAnterior y resetearAnterior.*/
+void ManipularBotones()
+{
+  subirAnterior = Subir(SUBIR, subirAnterior);
+  bajarAnterior = Bajar(BAJAR, bajarAnterior);
+  resetearAnterior = Resetear(RESETEAR, resetearAnterior);
+}
+
+
+/*Aumenta el valor de las unidades o decenas de 1 en 1
+cuando el pulsador es presionado. Si el valor de las unidades
+alcanza el valor de 10, unidades vuelve a valer cero
+y decenas aumenta un valor. 
+La función retorna el estado del botón*/
+int Subir(int pulsador, int estadoAntBoton)
+{
+  int estadoBoton = digitalRead(pulsador);
+  int cambio = !estadoBoton && estadoAntBoton;
+  if (cambio)
+  {
+    unidades++;
+    if (unidades == 10)
+    {
+      unidades = 0;
+      decenas++;
+      if (decenas == 10)
+      {
+        decenas = 0;
+      }
+    }
+  }
+  return estadoBoton;
+}
+
+
+/*Disminuye el valor de las unidades o decenas de 1 en 1 
+cuando el pulsador es presionado. Si el valor de las unidades
+alcanza el valor de 0, unidades pasa a valer 9 y 
+decenas disminuye un valor. 
+La función retorna el estado del botón*/
+int Bajar(int pulsador, int estadoAntBoton)
+{
+  int estadoBoton = digitalRead(pulsador);
+  int cambio = !estadoBoton && estadoAntBoton;
+  if (cambio) //Si presioné, entonces ejecutame.
+  {
+    unidades--;
+    if (unidades < 0)
+    {
+      unidades = 9;
+      decenas--;
+      if (decenas < 0)
+      {
+        decenas = 0;
+      }
+    }
+  }
+  return estadoBoton;
+}
+
+
+/*Reinicia los valores a cero de unidades y decenas 
+cuando el pulsador es presionado.
+La función retorna el estado del botón*/
+int Resetear(int pulsador, int estadoAntBoton)
+{
+  int estadoBoton = digitalRead(pulsador);
+  int cambio = !estadoBoton && estadoAntBoton;
+  if (cambio)
+  {
+    unidades = 0;
+    decenas = 0;
+  }
+  return estadoBoton;
+}
+
+
+/*Enciende los segmentos del display pasado por parámetro.
+Utiliza la función MostrarNumero() para encender los
+segmentos correspondientes al número ingresado por parámetro*/
+void PrenderDisplay(int display, int numero)
+{
+  if (display == UNIDADES)
+  {
+    digitalWrite(DECENAS, HIGH);
+    digitalWrite(UNIDADES, LOW);
+  }
+  else if (display == DECENAS)
+  {
+    digitalWrite(DECENAS, LOW);
+    digitalWrite(UNIDADES, HIGH);
+  }
+  else
+  {
+    digitalWrite(DECENAS, LOW);
+    digitalWrite(UNIDADES, LOW);
+  }
+  MostrarNumero(numero);
+}
+
+
+/*Enciende los segmentos del display según el número pasado por parámetro.
+Utiliza la función ApagarDisplay() para apagar todos los segmentos
+antes de encender los segmentos correspondientes.*/
+void MostrarNumero(int numero)
+{
+  ApagarDisplay();
+  switch (numero)
+  {
+    case 0:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    break;
+    case 1:
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    break;
+    case 2:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(G, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(D, HIGH);
+    break;
+    case 3:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+    case 4:
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+    case 5:
+    digitalWrite(A, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+    case 6:
+    digitalWrite(A, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+    case 7:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    break;
+    case 8:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+    case 9:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+  }
+}
+
+
+/*Apaga todos los segmentos del display*/
+void ApagarDisplay()
+{
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);	
+}
+
+
+
+
+
+
+
+
